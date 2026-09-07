f3e349ba2db02fe10b230954b068d2b7acbc463f
Akhilagouru <akhila.gouru10@gmail.com>
2026-09-07 18:04:38 +0530

    added window handles

--- src/test/java/WindowHandles.java
+++ src/test/java/WindowHandles.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
@@ -13,7 +15,20 @@ public class WindowHandles extends DemoqaBaseClass
 		{
 			  scrollDown();
 			  driver.findElement(By.xpath("//*[normalize-space()='Alerts, Frame & Windows']")).click();
-			  driver.findElement(By.xpath("//*[normalize-space()='Browser Windows']")).click();	  
+			  driver.findElement(By.xpath("//*[normalize-space()='Browser Windows']")).click();
+               
+			  String parentWindow = driver.getWindowHandle();
+			  driver.findElement(By.id("windowButton")).click();
+			  Set<String> windoHandles = driver.getWindowHandles();
+			  for(String childWindow : windoHandles) 
+			  {
+				  if(!childWindow.equals(parentWindow))
+				  {
+				 driver.switchTo().window(childWindow);
+				 System.out.println("child window is: " + childWindow+driver.getTitle() );
+				 driver.close();
+				 }
+			  }
 		}
 		catch(Exception e)
 		{
